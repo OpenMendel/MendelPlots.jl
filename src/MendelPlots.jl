8a001ff737779b5d669275bbc124e3a1d806c320
chris-german <chrisagerman@gmail.com>
2019-01-08 14:42:22 -0800

    Update MendelPlots.jl

--- src/MendelPlots.jl
+++ src/MendelPlots.jl
@@ -2,17 +2,15 @@ __precompile__()
 
 module MendelPlots
 
-using Gadfly, Compose, Statistics, DataFrames, Distributions, Reexport, DataFrames
+using Compose, Statistics, DataFrames, Distributions, Reexport, DataFrames
 @reexport using Gadfly
 import Fontconfig, Cairo
 
 export
-    # functions
     qq,
     manhattan,
     ppoints
-
-
+    
 include("gwasplots.jl")
 
 end
