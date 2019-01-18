813e4a99cfdf47d3db55b3a51c150caadced6db9
Chris German <germc3@ucla.edu>
2019-01-17 17:28:26 -0800

    Changed manhattan plot code to allow basepairs, allows skipped chomosomes.

--- src/gwasplots.jl
+++ src/gwasplots.jl
@@ -172,52 +172,95 @@ function manhattan(df::DataFrame; titles::AbstractString = "Manhattan Plot",
     ylabel::AbstractString = "-log<sub>10</sub>(p)", ymax::Union{Float64, Int64} = 0,
     signifline::Union{Float64, Int64} = -1, linecolor = "deepskyblue1", kwargs...)
 
-    df[:SNPnumber] = collect(1:size(df)[1])
+    using_basepairs = :pos in names(df)
+
     df[:log10pval] = -log10.(df[:pval])
     if ymax == 0
         ymax = ceil(maximum(df[:log10pval])) + 2.5
     end
     yticks = collect(0:2.5:ymax)
-    xlabs = string.(collect(1:2:length(unique(df[:chr]))))
-    xticks = Vector{Float64}(undef, length(xlabs))
+
     if typeof(df[:chr][1]) != String
         df[:chr] = string.(df[:chr])
-    end
-    global counter = 1
-    for i in collect(1:2:length(unique(df[:chr])))
-        xticks[counter] = mean(df[:SNPnumber][df[:chr] .== string.(i)])
-        global counter = counter + 1
+        xlabs = string.(parse.(Int64, unique(df[:chr]))[1:2:end])
+    else
+        xlabs = parse.(Int64, unique(df[:chr]))[1:2:end]
     end
 
     if signifline == -1
         signifline = -log10(0.05 / length(df[:chr]))
     end
 
-    function convertlabs(i)
-        return df[:chr][findfirst(df[:SNPnumber] .== round(i))]
+    if using_basepairs
+        a = by(df, :chr, max_pos = :pos => maximum)
+        a[:bp_add] = cumsum(a[:max_pos]) - a[:max_pos]
+        df = join(df, a, on  = :chr)
+        df[:adj_bp] = df[:pos] .+ df[:bp_add]
+        xticks = by(df, :chr, d -> (maximum(d.adj_bp) + minimum(d.adj_bp))/2)[2][1:2:end]
+
+        function convertlabsBP(i)
+            return string.(xlabs)[findfirst(xticks .== i)]
+        end
+    else
+        xticks = Vector{Float64}(undef, length(xlabs))
+        df[:SNPnumber] = collect(1:size(df)[1])
+        global counter = 1
+        for i in parse.(Int64, unique(df[:chr]))[1:2:end]
+            xticks[counter] = mean(df[:SNPnumber][df[:chr] .== string.(i)])
+            global counter = counter + 1
+        end
+        function convertlabs(i)
+            return df[:chr][findfirst(df[:SNPnumber] .== round(i))]
+        end
     end
+
+
     
     #make the manhattan plot
-    if length(unique(df[:chr])) == 22
-        plt1 = plot(df, x = :SNPnumber, y = :log10pval, color = :chr, Geom.point,
-            Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabs),
-            Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
-            Geom.abline(color = linecolor), Guide.ylabel(ylabel),
-            Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm,
-            key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
-            Guide.yticks(ticks = yticks), Scale.color_discrete_manual("#d54359", "#5ab543", 
-            "#a162dc", "#a9b245", "#5861cf", "#d89c39", "#6e8be3",
-            "#c7522a", "#5ea1d5", "#dd418d", "#66b974", "#9c41a3",
-            "#5a7936", "#df6cce", "#48b5a3", "#aa4584", "#997134", 
-            "#655fa0", "#dd8a69", "#bf8cd3", "#a24b5e", "#df83a3"); kwargs...);
+    if using_basepairs
+        if length(unique(df[:chr])) == 22
+            plt1 = plot(df, x = :adj_bp, y = :log10pval, color = :chr, Geom.point,
+                Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabsBP),
+                Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
+                Geom.abline(color = linecolor), Guide.ylabel(ylabel),
+                Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm,
+                key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
+                Guide.yticks(ticks = yticks), Scale.color_discrete_manual("#d54359", "#5ab543", 
+                "#a162dc", "#a9b245", "#5861cf", "#d89c39", "#6e8be3",
+                "#c7522a", "#5ea1d5", "#dd418d", "#66b974", "#9c41a3",
+                "#5a7936", "#df6cce", "#48b5a3", "#aa4584", "#997134", 
+                "#655fa0", "#dd8a69", "#bf8cd3", "#a24b5e", "#df83a3"); kwargs...);
+        else
+            plt1 = plot(df, x = :adj_bp, y = :log10pval, color = :chr, Geom.point,
+                Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabsBP),
+                Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
+                Geom.abline(color = linecolor), Guide.ylabel(ylabel),
+                Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm, 
+                key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
+                Guide.yticks(ticks = yticks), Scale.color_discrete; kwargs...);
+        end
     else
-        plt1 = plot(df, x = :SNPnumber, y = :log10pval, color = :chr, Geom.point,
-            Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabs),
-            Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
-            Geom.abline(color = linecolor), Guide.ylabel(ylabel),
-            Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm, 
-            key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
-            Guide.yticks(ticks = yticks), Scale.color_discrete; kwargs...);
+        if length(unique(df[:chr])) == 22
+            plt1 = plot(df, x = :SNPnumber, y = :log10pval, color = :chr, Geom.point,
+                Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabs),
+                Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
+                Geom.abline(color = linecolor), Guide.ylabel(ylabel),
+                Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm,
+                key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
+                Guide.yticks(ticks = yticks), Scale.color_discrete_manual("#d54359", "#5ab543", 
+                "#a162dc", "#a9b245", "#5861cf", "#d89c39", "#6e8be3",
+                "#c7522a", "#5ea1d5", "#dd418d", "#66b974", "#9c41a3",
+                "#5a7936", "#df6cce", "#48b5a3", "#aa4584", "#997134", 
+                "#655fa0", "#dd8a69", "#bf8cd3", "#a24b5e", "#df83a3"); kwargs...);
+        else
+            plt1 = plot(df, x = :SNPnumber, y = :log10pval, color = :chr, Geom.point,
+                Guide.xticks(ticks = xticks), Guide.xlabel(xlabel), Scale.x_continuous(labels = convertlabs),
+                Geom.abline(), intercept=[signifline], slope = [0], Guide.title(titles), 
+                Geom.abline(color = linecolor), Guide.ylabel(ylabel),
+                Theme(panel_fill = nothing, highlight_width = 0mm, point_size = 0.5mm, 
+                key_position = :none, grid_line_width = 0mm, panel_stroke = colorant"black"),
+                Guide.yticks(ticks = yticks), Scale.color_discrete; kwargs...);
+        end
     end
     draw(PNG(outfile, 6inch, 4inch, dpi = dpi), plt1);
 end
